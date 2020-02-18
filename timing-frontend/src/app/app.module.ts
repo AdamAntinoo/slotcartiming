f158f628564cf15cf1a7962322d4b65e1fbfe9db
Adam Antinoo <adamantinoo.git@gmail.com>
2020-02-18 13:47:57 +0100

    -[FRONT][BUG] Fix the rendering for times when they are not set. Not numbers so the formatting should go to the code.

--- timing-frontend/src/app/app.module.ts
+++ timing-frontend/src/app/app.module.ts
@@ -5,16 +5,20 @@ import { AppRoutingModule } from './app-routing.module';
 import { AppComponent } from './app.component';
 import { LaneTimingPanelComponent } from './panels/lane-timing-panel/lane-timing-panel.component';
 
+import { registerLocaleData } from '@angular/common';
+import localeEs from '@angular/common/locales/es';
+registerLocaleData(localeEs, 'es-ES');
+
 @NgModule({
-  declarations: [
-    AppComponent,
-    LaneTimingPanelComponent
-  ],
-  imports: [
-    BrowserModule,
-    AppRoutingModule
-  ],
-  providers: [],
-  bootstrap: [AppComponent]
+    declarations: [
+        AppComponent,
+        LaneTimingPanelComponent
+    ],
+    imports: [
+        BrowserModule,
+        AppRoutingModule
+    ],
+    providers: [],
+    bootstrap: [AppComponent]
 })
 export class AppModule { }
--- timing-frontend/src/app/domain/LaneTimingData.domain.ts
+++ timing-frontend/src/app/domain/LaneTimingData.domain.ts
@@ -1,34 +1,40 @@
 // - DOMAIN
 import { LapTimeRecord } from './LapTimeRecord.domain';
 import { DSTransmissionRecord } from './dto/DSTransmissionRecord.dto';
+import { formatNumber } from '@angular/common';
+import localeEs from '@angular/common/locales/es';
 
 const FAILED_LAP_TIME_LIMIT = 40.0;
 
 export class LaneTimingData {
+    public clean: boolean = true;
     public lane: number = 0;
     public lapCount: number = 0;
     public lapSplitCount: number = 0;
     public bestSplitTime: number = 9999.0;
     public lapTimeRecords: LapTimeRecord[] = [];
     public bestTime: LapTimeRecord = new LapTimeRecord();
-    public averageTime: number;
+    public averageTime: number = 0.0;
 
     // - G E T T E R S   &   S E T T E R S
     public setLaneNumber(lane: number): LaneTimingData {
         this.lane = lane;
         return this;
     }
-    public getLapCount(): string {
-        return this.lapCount + '';
-        // if (this.lapCount == 0) return '-';
-        // if ((this.lapCount - this.lapReset) == 0) return '-';
-        // return (this.lapCount - this.lapReset) + '';
+    public getBestTime(): string {
+        if (this.bestTime.time == 9999.0) return '-.-';
+        return formatNumber(this.bestTime.time, 'es-ES', '2.4-4');
     }
     public getBestSplitTime(): string {
         if (this.bestSplitTime == 9999.0) return '-.-';
         return this.bestSplitTime + '';
     }
+    public getAverageTime(): string {
+        if (this.averageTime == 0.0) return '-.-';
+        return formatNumber(this.averageTime, 'es-ES', '2.4-4');
+    }
     public processEvent(event: DSTransmissionRecord): void {
+        this.clean = false;
         this.lapCount++; // Update the lap count.
         let timeRecord = this.extractTime(event); // Get the lap time.
         let newTimeRecord = new LapTimeRecord({
